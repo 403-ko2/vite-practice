bf39cf8436794e13c8a197daa04e1df83db3e5ae
403-ko2 <zaekotu@gmail.com>
2024-11-25 12:03:09 -0500

    fixed the data import problem '

---
+++ app/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import data from './data.json';
+import { v4 as generateUUID } from 'uuid'; 
+console.log(data);
+console.log(generateUUID())
+
+/*
+1. delete everything in main
+2. delete everything in style
+3. delete the counter.js file
+4. delete the javascript.svg file
+5. delete the public/vite/svg file
+*/
